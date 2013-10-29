3e3dc504a3e80b70311e5d43741a27b46550354e
sponte03 <sponte03@SPONTE031.SPONTECORP.INTERNAL>
2013-10-29 14:59:09 -0430

    Método para hacer Restore  de la Base de Datos.

--- ferreasesorweb/src/java/com/spontecorp/ferreasesor/controller/BackUpBeanController.java
+++ ferreasesorweb/src/java/com/spontecorp/ferreasesor/controller/BackUpBeanController.java
@@ -23,58 +23,88 @@ import javax.faces.event.ActionEvent;
 public class BackUpBeanController implements Serializable {
 
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-    
+    //Datos de la BD a respaldar
+    private String dbName = JpaUtilities.DB_NAME;
+    private String dbUserName = JpaUtilities.DB_USER;
+    private String dbPassword = JpaUtilities.DB_PASSWORD;
+    //Ruta para crear el archivo .sql
+    private String savePath = "C:\\ferreasesorBackUpDB\\";
+    //Nombre del archivo .sql a crear
+    private String nameFile = "ferreasesorDB_" + sdf.format((new Date())) + ".sql";
+    //Ruta del mysqldump.exe 
+    private String mysqldump = "\"C:\\Program Files\\MySQL\\MySQL Server 5.1\\bin\\mysqldump.exe\"";
+    //Ruta del mysql.exe 
+    String mysql = "\"C:\\Program Files\\MySQL\\MySQL Server 5.1\\bin\\mysql.exe\"";
+
     public BackUpBeanController() {
     }
 
     /**
-     * BackUp DB
-     * Método para crear Respaldo de la Base de Datos
+     * BackUp de la DB Método para crear Respaldo de la Base de Datos
+     *
      * @param evt
-     * @return 
+     * @return
      */
     public boolean backUpDB(ActionEvent evt) {
-        
+
         boolean result = false;
-        
-        //Ruta para crear el archivo .sql
-        String savePath = "C:\\ferreasesorBackUpDB\\";
 
-        //Nombre del archivo .sql a crear
-        String nameFile = "backUpDB_"+ sdf.format((new Date())) + ".sql";
-        
-        //Ruta del mysqldump.exe 
-        String mysqldump = "\"C:\\Program Files\\MySQL\\MySQL Server 5.1\\bin\\mysqldump.exe\"";
-        
-        //Datos de la BD a respaldar
-        String dbName = JpaUtilities.DB_NAME;
-        String dbUserName = JpaUtilities.DB_USER;
-        String dbPassword = JpaUtilities.DB_PASSWORD;
-        
         //Se arma el comando a ejecutar
-        String executeCmd = mysqldump + " -u " + dbUserName + " -p" + dbPassword 
+        String executeCmd = mysqldump + " -u " + dbUserName + " -p" + dbPassword
                 + " --add-drop-database -B " + dbName + " -r " + savePath + nameFile;
-        
-        Process runtimeProcess;
 
+        Process runtimeProcess;
         try {
             runtimeProcess = Runtime.getRuntime().exec(executeCmd);
             int processComplete = runtimeProcess.waitFor();
 
             if (processComplete == 0) {
                 //System.out.println("Backup created successfully");
-                JsfUtil.addSuccessMessage("Backup de la Base de Datos creado con éxito! "
+                JsfUtil.addSuccessMessage("Respaldo de la Base de Datos creado con éxito! "
                         + "Ruta del archivo creado: " + savePath + nameFile);
                 result = true;
             } else {
                 //System.out.println("Could not create the backup");
-                JsfUtil.addErrorMessage("No se pudo crear el Backup de la Base de Datos.");
+                JsfUtil.addErrorMessage("No se pudo crear el Respaldo de la Base de Datos.");
                 result = false;
             }
         } catch (IOException | InterruptedException ex) {
-            System.out.println("Error al crear el Backup de la Base de Datos: " + ex.getMessage());
+            System.out.println("Error al crear el Respaldo de la Base de Datos: " + ex.getMessage());
         }
         return result;
     }
 
+    /**
+     * Restore de la DB Método para Restaurar la Base de Datos
+     *
+     * @param evt
+     * @return
+     */
+    public boolean restoreDB(ActionEvent evt) {
+
+        boolean result = false;
+
+        //Se arma el comando a ejecutar
+        String[] executeCmd = new String[]{mysql, "--user=" + dbUserName, "--password=" 
+                + dbPassword, dbName, "-e", "source " + savePath + nameFile};
+
+        Process runtimeProcess;
+        try {
+
+            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            if (processComplete == 0) {
+                JsfUtil.addSuccessMessage("La Base de Datos fue restaurada con éxito!");
+                result = true;
+            } else {
+                JsfUtil.addErrorMessage("No se pudo restaurar la Base de Datos.");
+                result = false;
+            }
+        } catch (IOException | InterruptedException ex) {
+            System.out.println("Error al restaurar la Base de Datos: " + ex.getMessage());
+        }
+
+        return result;
+    }
 }
