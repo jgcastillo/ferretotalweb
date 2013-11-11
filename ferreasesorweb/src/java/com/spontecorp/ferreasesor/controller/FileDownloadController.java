7ccf982177cc25826b32972f5edbcc94e6c28985
sponte03 <sponte03@SPONTE031.SPONTECORP.INTERNAL>
2013-11-11 16:42:00 -0430

    BackUp implementado usando ProcessBuilder.

--- ferreasesorweb/src/java/com/spontecorp/ferreasesor/controller/FileDownloadController.java
+++ ferreasesorweb/src/java/com/spontecorp/ferreasesor/controller/FileDownloadController.java
@@ -8,17 +8,18 @@ import com.spontecorp.ferreasesor.controller.util.JsfUtil;
 import com.spontecorp.ferreasesor.entity.ConfigurationDb;
 import com.spontecorp.ferreasesor.jpa.ConfigurationDbFacade;
 import com.spontecorp.ferreasesor.utilities.JpaUtilities;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.naming.InitialContext;
@@ -42,9 +43,11 @@ public final class FileDownloadController implements Serializable {
     private String dbUserName = null;
     private String dbPassword = null;
     private String mysqldump = null;
-    private String savePath = null;
+    private String filepath = null;
+    private String host = null;
+    private String port = null;
     //Nombre del archivo .sql a crear
-    private String nameFile = null;
+    private String filename = null;
 
     public FileDownloadController() {
         boolean findConfig = configurarDB();
@@ -57,8 +60,9 @@ public final class FileDownloadController implements Serializable {
     }
 
     /**
-     * Busco la configuración de la Base de Datos
-     * @return 
+     * Obtengo la configuración de la Base de Datos
+     *
+     * @return
      */
     public boolean configurarDB() {
         List<ConfigurationDb> configuracionDB = new ArrayList<>();
@@ -70,14 +74,15 @@ public final class FileDownloadController implements Serializable {
             configuracionDB = configuracionDBFacade.findAll();
             if (configuracionDB.size() > 0) {
                 configDB = configuracionDB.get(0);
+                host = "localhost";
+                port = "3306";
                 dbName = configDB.getDbName();
-                System.out.println("dbName: "+dbName);
                 dbUserName = configDB.getDbuserName();
-                System.out.println("dbUserName: "+dbUserName);
                 dbPassword = configDB.getDbPassword();
-                System.out.println("dbPassword: "+dbPassword);
+                // Set path. Set location of mysqldump        
                 mysqldump = "\"" + configDB.getPathMysqldump() + "\"";
-                System.out.println("mysqldump: "+mysqldump);
+                filename = "ferreasesorDB_" + sdf.format((new Date())) + ".sql";
+                filepath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/temp/" + filename);
                 isConfig = true;
             } else {
                 isConfig = false;
@@ -101,39 +106,121 @@ public final class FileDownloadController implements Serializable {
      */
     public void backUpDB() {
 
-        boolean result = false;
-        nameFile = "ferreasesorDB_" + sdf.format((new Date())) + ".sql";
-        savePath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/temp/" + nameFile);
+        //Se arma el comando a ejecutar para probar que funciona 
+        //la creación del BackUp vía Cónsola (cmd)
+        //String executeCmd = mysqldump + " -u " + dbUserName + " -p" + dbPassword
+        //        + " --add-drop-database -B " + dbName + " -r " + filepath;
+        //System.out.println("executeCmd: " + executeCmd);
 
-        //Se arma el comando a ejecutar
-        String executeCmd = mysqldump + " -u " + dbUserName + " -p" + dbPassword
-                + " --add-drop-database -B " + dbName + " -r " + savePath;
-        
-        System.out.println("executeCmd: "+executeCmd);
+        //Llamo al método que crea el archivo con el Back Up
+        boolean success = BackupDatabase();
 
-        Process runtimeProcess;
-        try {
-            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-            int processComplete = runtimeProcess.waitFor();
+        if (success) {
+            //System.out.println("Backup created successfully");
+            JsfUtil.addSuccessMessage("Respaldo de la Base de Datos creado con éxito! "
+                    + "Ruta del archivo creado: " + filepath);
 
-            if (processComplete == 0) {
-                //System.out.println("Backup created successfully");
-                JsfUtil.addSuccessMessage("Respaldo de la Base de Datos creado con éxito! "
-                        + "Ruta del archivo creado: " + savePath);
-                result = true;
+            InputStream stream = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/resources/temp/" + filename);
+            downloadFile = new DefaultStreamedContent(stream, "application/octet-stream", filename);
 
-                InputStream stream = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/resources/temp/" + nameFile);
-                downloadFile = new DefaultStreamedContent(stream, "application/octet-stream", nameFile);
-
-                //Elimino el archivo de la Carpeta Temporal
-                JpaUtilities.deleteFile(savePath);
-            } else {
-                //System.out.println("Could not create the backup");
-                JsfUtil.addErrorMessage("No se pudo crear el Respaldo de la Base de Datos.");
-                result = false;
-            }
-        } catch (IOException | InterruptedException ex) {
-            System.out.println("Error al crear el Respaldo de la Base de Datos: " + ex.getMessage());
+            //Elimino el archivo de la Carpeta Temporal
+            JpaUtilities.deleteFile(filepath);
+        } else {
+            //System.out.println("Could not create the backup");
+            JsfUtil.addErrorMessage("No se pudo crear el Respaldo de la Base de Datos.");
         }
+    }
+
+    /**
+     * Método para crear el archivo con el Back Up
+     *
+     * @return
+     */
+    public boolean BackupDatabase() {
+
+        boolean success = false;
+        try {
+            // Get SQL DUMP data
+            String dump = getServerDumpData(host, port, dbUserName, dbPassword, dbName);
+            if (!dump.isEmpty()) {
+                byte[] data = dump.getBytes("UTF-8");
+                // Set backup folder
+                String rootpath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/temp/");
+                // See if backup folder exists
+                File file = new File(rootpath);
+                if (!file.isDirectory()) {
+                    // Create backup folder when missing. Write access is needed.
+                    file.mkdir();
+                }
+                // Write SQL DUMP file
+                File filedst = new File(filepath);
+                FileOutputStream dest = new FileOutputStream(filedst);
+                dest.write(data);
+                dest.close();
+                success = true;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Error al crear el Respaldo de la Base de Datos BackupDatabase(): " + ex.getMessage());
+        }
+        return success;
+    }
+
+    /**
+     * Get SQL DUMP data
+     *
+     * @param host
+     * @param port
+     * @param user
+     * @param password
+     * @param db
+     * @return
+     */
+    public String getServerDumpData(String host, String port, String user, String password, String db) {
+        StringBuilder dumpdata = new StringBuilder();
+        int STREAM_BUFFER = 512000;
+        try {
+            if (host != null && user != null && password != null && db != null) {
+
+                // Usage: mysqldump [OPTIONS] database [tables]
+                // OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
+                // OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
+                String command[] = new String[]{mysqldump,
+                    "--host=" + host,
+                    "--port=" + port,
+                    "--user=" + user,
+                    "--password=" + password,
+                    "--compact",
+                    "--complete-insert",
+                    "--extended-insert",
+                    "--skip-comments",
+                    "--skip-triggers",
+                    db};
+
+                // Run mysqldump
+                ProcessBuilder pb = new ProcessBuilder(command);
+                Process process = pb.start();
+
+                InputStream in = process.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+                int count;
+                char[] cbuf = new char[STREAM_BUFFER];
+
+                // Read datastream
+                while ((count = br.read(cbuf, 0, STREAM_BUFFER)) != -1) {
+                    dumpdata.append(cbuf, 0, count);
+                }
+                // Close
+                br.close();
+                in.close();
+            }
+
+        } catch (Exception ex) {
+            // Handle exception as you wish
+            System.out.println("Error al crear el Respaldo de la Base de Datos getServerDumpData(): " + ex.getMessage());
+            return "";
+        }
+        return dumpdata.toString();
     }
 }
